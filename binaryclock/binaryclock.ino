71efb22f7b9f1e17461bc44c4ec889628e6197f4
Stefan Hüsges <tronsha@gmx.de>
2015-09-07 20:24:01 +0200

    update

--- binaryclock/binaryclock.ino
+++ binaryclock/binaryclock.ino
@@ -5,6 +5,8 @@ RTC_DS1307 rtc;
 
 void shiftOutTime(DateTime);
 void shiftOutDate(DateTime);
+void shiftOutSetup(DateTime, int);
+void setupDateTime(DateTime, int);
 
 int pinClock2 = 2;
 int pinData2 = 3;
@@ -55,46 +57,10 @@ void loop () {
     count--;
     if (digitalRead(pinSetup) == LOW) {
       count = 10;
-      if (mode == 0b00000001) {
-        rtc.adjust(DateTime(now.year(), now.month(), now.day(), now.hour() == 23 ? 0 : now.hour() + 1, now.minute(), now.second()));
-      }
-      if (mode == 0b00000010) {
-        rtc.adjust(DateTime(now.year(), now.month(), now.day(), now.hour(), now.minute() == 59 ? 0 : now.minute() + 1, now.second()));
-      }
-      if (mode == 0b00000100) {
-        rtc.adjust(DateTime(now.year(), now.month(), now.day(), now.hour(), now.minute(), now.second() == 59 ? 0 : now.second() + 1));
-      }
-      if (mode == 0b00001000) {
-        rtc.adjust(DateTime(now.year() == 2060 ? 2015 : now.year() + 1, now.month(), now.day(), now.hour(), now.minute(), now.second()));
-      }
-      if (mode == 0b00010000) {
-        rtc.adjust(DateTime(now.year(), now.month() == 12 ? 1 : now.month() + 1, now.day(), now.hour(), now.minute(), now.second()));
-      }
-      if (mode == 0b00100000) {
-        rtc.adjust(DateTime(now.year(), now.month(), now.day() == 31 ? 1 : now.day() + 1, now.hour(), now.minute(), now.second()));
-      }
+      setupDateTime(now, mode);
       now = rtc.now();
     }
-    shiftOut(pinData2, pinClock2, MSBFIRST, mode);
-    shiftOut(pinData1, pinClock1, MSBFIRST, 0);
-    if (mode == 0b00000001) {
-      shiftOut(pinData0, pinClock0, MSBFIRST, now.hour());
-    }
-    if (mode == 0b00000010) {
-      shiftOut(pinData0, pinClock0, MSBFIRST, now.minute());
-    }
-    if (mode == 0b00000100) {
-      shiftOut(pinData0, pinClock0, MSBFIRST, now.second());
-    }
-    if (mode == 0b00001000) {
-      shiftOut(pinData0, pinClock0, MSBFIRST, now.year() - 2000);
-    }
-    if (mode == 0b00010000) {
-      shiftOut(pinData0, pinClock0, MSBFIRST, now.month());
-    }
-    if (mode == 0b00100000) {
-      shiftOut(pinData0, pinClock0, MSBFIRST, now.day());
-    }
+    shiftOutSetup(now, mode);
   } else {
     mode = 0b01000000;
     if (digitalRead(pinSetup) == LOW) {
@@ -119,4 +85,47 @@ void shiftOutTime(DateTime now) {
   shiftOut(pinData2, pinClock2, MSBFIRST, now.hour());
 }
 
+void shiftOutSetup(DateTime now, int mode) {
+  shiftOut(pinData2, pinClock2, MSBFIRST, mode);
+  shiftOut(pinData1, pinClock1, MSBFIRST, 0);
+  if (mode == 0b00000001) {
+    shiftOut(pinData0, pinClock0, MSBFIRST, now.hour());
+  }
+  if (mode == 0b00000010) {
+    shiftOut(pinData0, pinClock0, MSBFIRST, now.minute());
+  }
+  if (mode == 0b00000100) {
+    shiftOut(pinData0, pinClock0, MSBFIRST, now.second());
+  }
+  if (mode == 0b00001000) {
+    shiftOut(pinData0, pinClock0, MSBFIRST, now.year() - 2000);
+  }
+  if (mode == 0b00010000) {
+    shiftOut(pinData0, pinClock0, MSBFIRST, now.month());
+  }
+  if (mode == 0b00100000) {
+    shiftOut(pinData0, pinClock0, MSBFIRST, now.day());
+  }
+}
+
+void setupDateTime(DateTime now, int mode) {
+  if (mode == 0b00000001) {
+    rtc.adjust(DateTime(now.year(), now.month(), now.day(), now.hour() == 23 ? 0 : now.hour() + 1, now.minute(), now.second()));
+  }
+  if (mode == 0b00000010) {
+    rtc.adjust(DateTime(now.year(), now.month(), now.day(), now.hour(), now.minute() == 59 ? 0 : now.minute() + 1, now.second()));
+  }
+  if (mode == 0b00000100) {
+    rtc.adjust(DateTime(now.year(), now.month(), now.day(), now.hour(), now.minute(), now.second() == 59 ? 0 : now.second() + 1));
+  }
+  if (mode == 0b00001000) {
+    rtc.adjust(DateTime(now.year() == 2060 ? 2015 : now.year() + 1, now.month(), now.day(), now.hour(), now.minute(), now.second()));
+  }
+  if (mode == 0b00010000) {
+    rtc.adjust(DateTime(now.year(), now.month() == 12 ? 1 : now.month() + 1, now.day(), now.hour(), now.minute(), now.second()));
+  }
+  if (mode == 0b00100000) {
+    rtc.adjust(DateTime(now.year(), now.month(), now.day() == 31 ? 1 : now.day() + 1, now.hour(), now.minute(), now.second()));
+  }
+}
 
